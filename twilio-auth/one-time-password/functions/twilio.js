41cea3e0ce95ccac40934c9062a29758fd2166ab
Esat Balkir <esatbalkir@yahoo.com>
2017-05-02 01:44:55 -0400

    removing sensitive info

--- twilio-auth/one-time-password/functions/twilio.js
+++ twilio-auth/one-time-password/functions/twilio.js
@@ -1,7 +1,5 @@
 const twilio = require('twilio');
-
-//gitignore (put them in another file, add to gitignore and require in this file)
-const accountSid = 'AC699c6a1d1a0bd7d671263ddbb3fe6b0a';
-const authToken = 'a409f65b992d837dac170aecbc7904c3';
+import accountSid from './twilio_account_sid.json';
+import authToken from './twilio_auth_token.json';
 
 module.exports = new twilio.Twilio(accountSid, authToken);
